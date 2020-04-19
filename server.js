567d4b7f4b64f7f57c1c7adca11961c4a141d2e1
Theresa E <t.eatherly@gmail.com>
2020-04-19 14:55:14 -0500

    delete notes function

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const util = require("util");
 const express = require("express");
 const uuid = require("uuid");
 // exported file to import in server.js
-const dbjson = require("./db/db.json");
+const dbjson= require("./db/db.json");
+console.log(uuid);
 
-
-const readFileAsync = util.promisify(fs.readFile);
+// const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
@@ -30,36 +30,59 @@ app.get("/notes", function(req, res) {
 });
 
 // ===========================================================
-// app.get("/api/notes", function(req, res) {
-//     return res.json(dbjson);
-//   });
+app.get("/api/notes", function(req, res) {
+    return res.json(dbjson);
+  });
 
-   app.get("/api/notes", function(req, res){
-    readFileAsync("./db/db.json", "utf8").then (data => {
-    const notesJSON = JSON.parse(data)
-    res.json(notesJSON)
-    })
-   });
+  //  app.get("/api/notes", function(req, res){
+  //   readFileAsync("./db/db.json", "utf8").then (data => {
+  //   const notesJSON = JSON.parse(data)
+  //   res.json(notesJSON)
+  //   })
+  //  });
 
-    app.post("/api/notes", function (req, res) {
+    app.post("/api/notes", async function (req, res) {
      let newNote = req.body
-    let id = uuid.v4
-    newNote.id = `${id}` 
-      readFileAsync("./db/db.json", "utf8").then (data => {
-      const notesJSON = JSON.parse(data);
-      notesJSON.push(newNote);
-    
-        writeFileAsync("./db/db.json", JSON.stringify(notesJSON)).then(() => {
+    let id = uuid.v4()
+    newNote.id = id
+    console.log(id);
+      // readFileAsync("./db/db.json", "utf8").then (data => {
+      // const notesJSON = JSON.parse(data);
+      dbjson.push(newNote);
+    try {
+      await writeFileAsync("./db/db.json", JSON.stringify(dbjson)); 
         res.json(newNote);
-        })
-    })
+         } catch (error) {
+      res.json(error)
+    }
   }); 
 
-  app.delete("/api/notes/:id", function(req, res){
-    dbjson.delete(req.body);
-    dbjson.length = 0;
-    res.json({ ok: true });
-  });
+  // promisify fsWritefile. This allows to use .then(which is a promise); therefore, await returns a promise. 
+  // Async "Awaits" for the return. Await has to be in ans asyncronys function
+  app.delete("/api/notes/:id", async function(req, res){
+    let deleteNote=req.params.id;
+    console.log(deleteNote);
+    // use filter arrary es6 66-71
+    let filteredArray = [];
+    for(var i = 0; i < dbjson.length; i++){
+      if (deleteNote !=dbjson[i].id){
+        filteredArray.push(dbjson[i])
+      }
+      }
+      // take place of .then (throws error if server crashes)
+      try {
+        await writeFileAsync("./db/db.json", JSON.stringify(filteredArray)); 
+          res.json(newNote);
+           } catch (error) {
+        res.json(error)
+      }
+      dbjson=filteredArray;
+      //  let  newNote = id(req.params.newNoteId).then(newNote => {
+      //     if(!newNote) {
+     
+    res.json({ ok: true });g
+  })
+ 
 
 
   // Listener
